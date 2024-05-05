7b28be374ecd522a17b2c0f81cb0a0b136f4bc65
Robert Scheidegger <robert_scheidegger@brown.edu>
2024-05-05 15:04:48 -0400

    feat: complete report!

--- src/bfv.py
+++ src/bfv.py
@@ -208,10 +208,111 @@ class BFV:
         """
         return (c_1[0] + c_2[0], c_1[1] + c_2[1])
 
+from bfv import *
+
+def test_ring_addition():
+    R = make_ring(5, 3)
+    a = R([1, 2, 3])
+    b = R([4, 5, 6])
+    c = a + b
+    assert c.c == [0, 2, 4]
+    
+def test_ring_multiplication():
+    R = make_ring(5, 3)
+    a = R([1, 0, 1])
+    b = R([0, 1, 0])
+    c = a * b
+    # (1 + x^2) x = x + x^3 = x - 1 = x + 4
+    assert c.c == [4, 1, 0]
+    
+def test_basic_bfv():
+    Q = 1382
+    T = 10
+    M = 3
+    bfv = BFV(5.0, Q, M, T)
+    sk, pk = bfv.generate_keypair()
+    
+    m = bfv.R_T([1, 2, 3])
+    
+    c = bfv.encrypt(pk, m)
+    assert bfv.decrypt(sk, c) == m
+    
+def test_medium_bfv():
+    Q = 1382413143
+    T = 1124
+    M = 6
+    bfv = BFV(10.0, Q, M, T)
+    sk, pk = bfv.generate_keypair()
+    
+    m = bfv.R_T([1, 2, 3, 4, 5, 6])
+    c = bfv.encrypt(pk, m)
+    assert bfv.decrypt(sk, c) == m
+    
+    # Test homomorphic addition
+    m2 = bfv.R_T([6, 5, 4, 3, 2, 1])
+    c2 = bfv.encrypt(pk, m2)
+    c3 = bfv.add(c, c2)
+    assert bfv.decrypt(sk, c3) == bfv.R_T([7, 7, 7, 7, 7, 7])
+    
+def test_random_addition():
+    Q = 10**10
+    T = 10**5
+    M = 100
+    bfv = BFV(10.0, Q, M, T)
+    
+    sk, pk = bfv.generate_keypair()
+    
+    for _ in range(10):
+        m = bfv.R_T([random.randint(0, T - 1) for _ in range(M)])
+        c = bfv.encrypt(pk, m)
+        assert bfv.decrypt(sk, c) == m
+        
+        m2 = bfv.R_T([random.randint(0, T - 1) for _ in range(M)])
+        c2 = bfv.encrypt(pk, m2)
+        c3 = bfv.add(c, c2)
+        assert bfv.decrypt(sk, c3) == (m + m2)
+    
+
 def make_graphs():
+    """
+    Makes the graphs that are included in the report.
+    
+    This will require you to have matplotlib installed.
+    """
     print("Running experiments and making graphs...")
     
     import matplotlib.pyplot as plt
+    
+    # Make a graph of the time taken to encrypt/decrypt as a function of $m$.
+    
+    Q = 1382413143
+    T = 1124
+    times = []
+    m_values = []
+    for M in range(10, 2000, 10):
+        bfv = BFV(10.0, Q, M, T)
+        sk, pk = bfv.generate_keypair()
+        
+        m = bfv.R_T([1] * M)
+        
+        import time
+        start = time.time()
+        
+        c = bfv.encrypt(pk, m)
+        
+        bfv.decrypt(sk, c)
+        end = time.time()
+        
+        times.append(end - start)
+        m_values.append(M)
+        
+        print(f"Finished M = {M} in {end - start} seconds.")
+    
+    plt.title("Time to Encrypt/Decrypt vs. M")
+    plt.xlabel("M")
+    plt.ylabel("Time (s)")
+    plt.plot(m_values, times)
+    plt.savefig("graphs/time_vs_m.png")
     
     
     
@@ -234,12 +335,20 @@ if __name__ == "__main__":
     # Encode the message into R_T
     m = bfv.R_T([ord(c) for c in args.m])
     
+    # Generate the keypair
     sk, pk = bfv.generate_keypair()
+    
+    # Encrypt the message
     c = bfv.encrypt(pk, m)
+    
+    # Decrypt the message
     m_prime = bfv.decrypt(sk, c)
+    
+    # Decode the message
+    m_prime_str = "".join([chr(c) for c in m_prime.c])
     
     print("Encoded Message:", m)
     print("Encrypted Message:", c)
     print("Decrypted Message:", m_prime)
-    print("Decoded Message:", "".join([chr(c) for c in m_prime.c]))
+    print("Decoded Message:", m_prime_str)
     
